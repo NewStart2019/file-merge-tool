0625ff26a4b005ea6315babcee97ad3f9715278c
zhanqh <2855942332@qq.com>
2024-10-09 16:08:26 +0800

    添加控制注释是否写入word功能

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 
 import argparse
-import os,re
+import os, re
 
 import docx
 from docx import Document
@@ -29,6 +29,7 @@ def read_java_files(directory, java_files, file_type):
             for dir in dirs:
                 read_java_files(dir, java_files, file_type)
 
+
 def set_text_style(doc, text):
     # 添加一段文本
     paragraph = doc.add_paragraph()
@@ -51,6 +52,7 @@ def set_text_style(doc, text):
     # 设置字体
     run.font.name = "monospace"
 
+
 def remove_java_coment(file_contents):
     # 移除单行注释
     code = re.sub(r'//.*', '', file_contents)
@@ -58,11 +60,13 @@ def remove_java_coment(file_contents):
     code = re.sub(r'/\*.*?\*/', '', code, flags=re.DOTALL)
     return code
 
+
 def remove_blank_lines(file_contents):
     # 将文本按行分割，并去除每行首尾的空白字符
     lines = [line for line in file_contents.splitlines() if line.strip()]
     # 将非空行重新组合成一个字符串
     return '\n'.join(lines)
+
 
 def remove_vue_comment(file_contents):
     # 移除单行注释
@@ -74,20 +78,23 @@ def remove_vue_comment(file_contents):
     code = re.sub(r'<\-\-\-+[^>]*\-\->', '', code, flags=re.DOTALL)
     return code
 
+
 # 读取每一个java文件
-def write_to_word(java_files, output_path, root, isWriteDir=True):
+def write_to_word(java_files, output_path, root, isWriteDir=True, noComment=False):
     doc = Document()
     dir = []
 
     for file in java_files:
         with open(file, "r", encoding="utf-8") as f:
             file_contents = f.read()
-        if file.endswith(".java"):
-            file_contents = remove_java_coment(file_contents)
-        elif file.endswith(".vue"):
-            file_contents = remove_vue_comment(file_contents)
-        # 正则匹配去掉空白行和注释
-        file_contents = remove_blank_lines(file_contents)
+        # 删除注释
+        if noComment:
+            if file.endswith(".java"):
+                file_contents = remove_java_coment(file_contents)
+            elif file.endswith(".vue"):
+                file_contents = remove_vue_comment(file_contents)
+            # 正则匹配去掉空白行和注释
+            file_contents = remove_blank_lines(file_contents)
         if isWriteDir:
             fileName = file.replace(root + "\\", '')
             # level = fileName.count("\\")
@@ -104,7 +111,7 @@ def write_to_word(java_files, output_path, root, isWriteDir=True):
         lastWwrite = ""
         for i in range(fileLine):
             paragraph = paragraphs[i].rstrip()
-            if (i + 1) < fileLine-1:
+            if (i + 1) < fileLine - 1:
                 nextParagraph = paragraphs[i + 1].strip()
             else:
                 nextParagraph = ""
@@ -133,6 +140,8 @@ def parseParameter():
     parser.add_argument('-f', '--fileType', nargs='+',
                         help='指定读取文件类型，可以-f java vue 指定多个文件，默认java文件')
 
+    parser.add_argument('-c', '--noComment', help='是否删除注释，默认不删除注释，true删除注释换行')
+
     # 解析命令行参数
     args = parser.parse_args()
     # 处理参数
@@ -148,6 +157,10 @@ def parseParameter():
         args.writeDir = (args.writeDir.lower()) == "true"
     else:
         args.writeDir = True
+    if args.noComment is not None:
+        args.noComment = (args.noComment.lower()) == "true"
+    else:
+        args.noComment = False
     if not args.fileType:
         args.fileType = ['java']
     return args
@@ -157,4 +170,4 @@ if __name__ == '__main__':
     args = parseParameter()
     read_java_files(args.target, java_files, args.fileType)
     # # 读取每一个文件写入到word文件中
-    write_to_word(java_files, args.output, args.target, args.writeDir)
+    write_to_word(java_files, args.output, args.target, args.writeDir, args.noComment)
